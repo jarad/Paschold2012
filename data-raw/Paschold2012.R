9d6f0b8dcf5170656c0e00df02f8ae78412c2032
Jarad Niemi <niemi@iastate.edu>
2016-04-25 10:35:13 -0500

    Fix Mo17xB3 -> Mo17xB73

--- data-raw/Paschold2012.R
+++ data-raw/Paschold2012.R
@@ -9,7 +9,7 @@ Paschold2012 = d %>%
   gather(tmp,count,-GeneID) %>%
   mutate(tmp_ = gsub("\\.","_",tmp)) %>%
   separate(tmp_, c("replicate","genotype","line")) %>%
-  mutate(genotype = plyr::revalue(genotype, c("B"="B73","M"="Mo17","BM"="B73xMo17","MB"="Mo17xB3")),
+  mutate(genotype = plyr::revalue(genotype, c("B"="B73","M"="Mo17","BM"="B73xMo17","MB"="Mo17xB73")),
          line = plyr::revalue(line, c("b"="B73","m"="Mo17")),
          replicate = substr(replicate,4,4),
          flow_cell = ceiling(as.numeric(replicate)/2)) %>%
